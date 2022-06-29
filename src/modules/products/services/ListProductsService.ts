f4c0ebad436e3e7f06f3fceba9d3767e26f85fa8
Welliton Goncalves De Souza <wsouza@cpqd.com.br>
2022-06-29 19:23:17 -0300

    Produto - create,update-delete

---
+++ src/modules/products/services/ListProductsService.ts
@@ -0,0 +1,12 @@
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    return productsRepository.find();
+  }
+}
+
+export default ListProductService;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,8 @@
+import productRouter from '@modules/products/routes/products.routes';
 import { Router } from 'express';
 
 const routes = Router();
+routes.use('/products', productRouter);
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello!' });
